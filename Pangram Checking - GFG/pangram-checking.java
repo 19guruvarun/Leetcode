a809fadeb38de20fe46ee84bebf69c3c1f8826b2
19guruvarun <95072264+19guruvarun@users.noreply.github.com>
2023-05-05 22:01:35 +0530

    Added solution - LeetHub

---
+++ Pangram Checking - GFG/pangram-checking.java
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+//Initial template for JAVA
+
+import java.lang.*;
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+//User function template for JAVA
+
+class Solution
+{
+    //Function to check if a string is Pangram or not.
+    public static boolean checkPangram  (String str) {
+        // your code here
+        for(char i='a';i<='z';i++){
+            if(!str.toLowerCase().contains(i+""))
+            return false;
+        }
+        return true;
+    }
+}
+
+//{ Driver Code Starts.
+
+class GFG
+ {
+	public static void main (String[] args) throws IOException
+	 {
+	  BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	  int t=Integer.parseInt(br.readLine());
+	  while(t-->0)
+	  {
+	    String s=br.readLine().trim();
+	    
+	    System.out.println(new Solution().checkPangram (s)==true?1:0);
+	  }
+	  
+	 }
+}
+// } Driver Code Ends
